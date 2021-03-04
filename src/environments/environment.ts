033620d7c93041e9699c1b35a618d03d47059a34
academy_learner <academy_learner@academy-learners-MacBook-Pro-3.local>
2021-03-04 16:01:18 +0200

    first changes that already have firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBOI-zmHjp9tjNb81I9e0M7Z_5dP8rky2M",
+    authDomain: "crud-firebase-80089.firebaseapp.com",
+    databaseURL: "https://crud-firebase-80089-default-rtdb.firebaseio.com",
+    projectId: "crud-firebase-80089",
+    storageBucket: "crud-firebase-80089.appspot.com",
+    messagingSenderId: "302881429146",
+    appId: "1:302881429146:web:350b54158c6125d6c17123",
+    measurementId: "G-NG9BWZSEDX"
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
